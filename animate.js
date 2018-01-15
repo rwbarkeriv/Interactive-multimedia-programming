f900bfdc05382fc04bc1490a9623246f79654400
rwbarkeriv <rwbarkeriv@gmail.com>
2018-01-15 10:06:44 +0200

    Added Lab #1 and overall layout

---
+++ animate.js
@@ -0,0 +1,15 @@
+function addName() {
+    var node = document.createElement("li");
+    var name = document.getElementById('name-input');
+    var textNode = document.createTextNode(name.value);
+    node.classList.add("list-group-item", "w-25");
+    node.appendChild(textNode);
+    document.getElementById("name-list").appendChild(node);
+    name.value = "";
+}
+
+function keyPress(event) {
+    if(event.keyCode == 13){
+        addName();
+    }
+}
